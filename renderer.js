c9ff329303bd067a93aa8f6475d2b864dded92a8
POIREL Paul <POIRELP@groupe.active>
2020-06-14 14:15:42 +0200

    gestion des programmations

--- renderer.js
+++ renderer.js
@@ -171,58 +171,126 @@ function displayClass(class_){
   console.log(class_)
   $('#className').val(class_.name)
   $('#classProgram').val(class_.program)
-  buildProgrammationsTable(class_)
-  $('#class').show()
   window.class_ = class_
+  buildProgrammationsTable()
+  $('#class').show()
 }
 
-function buildProgrammationsTable(class_){
-  let $header = $('<tr><th>Programmations</th></tr>')
+function buildProgrammationsTable(){
+  let class_ = window.class_
+  console.log(window.class_.programmations)
+  let $header = $('<tr><th colspan="2">Programmations</th></tr>')
   let $programmations = $('#programmations').empty()
     .append($header)
 
-  let $programmationRows = {}
-  for (var p in class_.programmations) {
-    if (class_.programmations.hasOwnProperty(p)) {
-      $programmationRows[p]=$(`<tr><th>${p}</th></tr>`).appendTo($programmations)
-    }
+  let $programmationRows = []
+
+  let buildProgGroupCell = function(name, progGroup, colspan){
+
   }
+
+  let row = 0
+  class_.programmations.forEach((progGroup, i) => {
+    let currentRow = row
+    if(progGroup.programmations){
+      progGroup.programmations.forEach((prog, j) => {
+        $programmationRows[row]=$(`<tr><th><span class="editable progName">${prog.name}</span></th></tr>`)
+          .css('background-color',prog.color)
+          .data('uuid',prog.uuid)
+          .data('xpath',prog.xpath)
+          .data('progGroupIndex',i)
+          .appendTo($programmations)
+        row++
+      });
+    }
+    else {
+      $programmationRows[row]=$(`<tr></tr>`)
+        .css('background-color',progGroup.color)
+        .data('uuid',progGroup.uuid)
+        .data('xpath',progGroup.xpath)
+        .data('progGroupIndex',i)
+      .appendTo($programmations)
+      row++
+    }
+    $programmationRows[currentRow].prepend(
+      $(`<th class="progGroup"><span class="editable progGroupName">${progGroup.name}</span><a href="#" class="addProg"><span class="ui-icon ui-icon-plus"></span></th>`)
+      .css('background-color',progGroup.color)
+      .data('uuid',progGroup.uuid)
+      .data('xpath',progGroup.xpath)
+      .data('progGroupIndex',i)
+      .attr('colspan',progGroup.programmations?.length ? 1 : 2)
+      .attr('rowspan',progGroup.programmations?.length)
+      .appendTo($programmations)
+    )
+  })
 
   let prevDay
   let prevWeek
   for (var d in class_.days) {
     if (class_.days.hasOwnProperty(d)) {
-      //console.log()
-      let day = $.datepicker.parseDate("yy-mm-dd",d)
+      let day = $.datepicker.parseDate("yymmdd",d)
       if(prevDay == null || day.getDay()<prevDay.getDay() || (day.getTime() - prevWeek.getTime() > 1000*60*60*24*6)){
         let f = $.datepicker.formatDate('d/m',day)
 
-        $header.append(`<th>${f}</th>`)
+        let $th = $(`<th>${f}</th>`).appendTo($header)
 
         let periodBreak = day.getTime() - prevWeek?.getTime() > 1000*60*60*24*13
         if(periodBreak)
           $th.addClass('periodBreak')
 
-        for (var name in class_.programmations) {
-          if (class_.programmations.hasOwnProperty(name)) {
 
-              let programmation = class_.programmations[name]
-              let $td = $('<td>').appendTo($programmationRows[name])
+          row = 0
+          class_.programmations.forEach((progGroup, i) => {
+            if(progGroup.programmations){
+              progGroup.programmations.forEach((prog, j) => {
+                let $td = $('<td>').appendTo($programmationRows[row])
+                if(periodBreak)
+                  $td.addClass('periodBreak')
+                row ++
+              });
+            }else{
+              let $td = $('<td>').appendTo($programmationRows[row])
               if(periodBreak)
                 $td.addClass('periodBreak')
-              if(programmation.progressions){
-                //todo
-              } else {
-                $('<a class="addProgression">+</a>').appendTo($td)
-              }
-          }
-        }
+              row ++
+            }
+
+          });
+
         prevWeek = day
       }
       prevDay = day
     }
   }
 
+
+  $('.editable',$programmations)
+    .append('<a href="#" class="ui-icon ui-icon-pencil">')
+    .on('click','.ui-icon-pencil',function(){
+      let $editable = $(this).parent()
+      let children = $editable.children()
+      let text = $editable.text()
+      $editable.data('oldText', text)
+      $editable.empty().append(`<input value="${text}"/><a href="#" class="ui-icon ui-icon-check"></a><a href="#" class="ui-icon ui-icon-close"></a>`)
+      $('input',$editable).focus().select()
+    })
+    .on('click','.ui-icon-close',function(){
+        let $editable = $(this).parent()
+        $editable.text($editable.data('oldText')).append('<a href="#" class="ui-icon ui-icon-pencil">')
+    })
+    .on('click','.ui-icon-check',function(){
+        let $editable = $(this).parent()
+        $editable.text($('input',$editable).val())
+          .append('<a href="#" class="ui-icon ui-icon-pencil">')
+          .trigger('change')
+    })
+    .on('keydown','input',function(e){
+      if(e.key=='Enter'){
+        $(this).nextAll('.ui-icon-check').trigger('click')
+      } else if(e.key=='Escape'){
+        $(this).nextAll('.ui-icon-close').trigger('click')
+      }
+    })
 }
 
 function loadPrograms(){
@@ -266,22 +334,28 @@ function addPeriodClick(){
     window.class_.days = window.class_.days || {}
     for(let d = periodStart; d <= periodEnd; d.setDate(d.getDate()+1)){
       if(validDays[d.getDay()]){
-        let k = $.datepicker.formatDate('yy-mm-dd',d)
+        let k = $.datepicker.formatDate('yymmdd',d)
         window.class_.days[k]=window.class_.days[k]||{}
       }
     }
-    console.log(window.class_)
   buildProgrammationsTable()
 }
 
 function addProgrammationClick(){
   let name = $('#programmationName').val()
-  if(!name) return
-  window.class_.programmations = window.class_.programmations || {}
-  if(window.class_.programmations.hasOwnProperty(name)) return
+  let uuid = $('a#selectCompetence').data('uuid')
+  let referenceXPath = $('#programmationColor').data('referenceXPath')
 
-  window.class_.programmations[name]={}
-  buildProgrammationsTable(window.class_  )
+  if(!name) return
+  window.class_.programmations = window.class_.programmations || []
+
+  window.class_.programmations.push({
+    name:name,
+    color:$('#programmationColor').val(),
+    uuid:$('a#selectProgramItem').data('uuid'),
+    xpath:$('a#selectProgramItem').data('referenceXPath')
+  })
+  buildProgrammationsTable()
 }
 
 function selectCompetenceClick(){
@@ -295,8 +369,27 @@ function selectCompetenceClick(){
     $('#programmationColor').val(competence.color)
     $(this).data('uuid',competence.uuid).data('referenceXPath',competence.referenceXPath)
   }
-  console.log(competence)
   return false
+}
+
+function classProgramChange(){
+  window.class_.program = $(this).val()
+}
+
+function addSubProgrammationClick(){
+  let progGroupIndex = $(this).parents('tr').data('progGroupIndex')
+  let progGroup = window.class_.programmations[progGroupIndex]
+
+  progGroup.programmations = progGroup.programmations || []
+  progGroup.programmations.push({
+    name:"nouvelle programmation",
+    color:progGroup.color
+  })
+  buildProgrammationsTable()
+}
+
+function editableEditClick(){
+  let $span = $(this).prev()
 }
 
 $(function(){
@@ -304,7 +397,9 @@ $(function(){
 
   $('#addPeriod').on('click',addPeriodClick)
   $('#addProgrammation').on('click',addProgrammationClick)
-  $('#selectCompetence').on('click',selectCompetenceClick)
+  $('#selectProgramItem').on('click',selectCompetenceClick)
+  $('#classProgram').on('change',classProgramChange)
+  $('#programmations').on('click','.progGroup a.addProg',addSubProgrammationClick)
 
   let path = "C:/Users/poirelp/AppData/Roaming/CClasse/storage/classes/toto.json"
     window.openClassFile(path).then(r=>{
